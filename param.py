cb4c14f1819ea3986718a588ef6fb7f6999ca727
qh1874 <qihan1874@163.com>
2024-10-27 16:36:35 +0800

    modified:   param.py

--- param.py
+++ param.py
@@ -2,7 +2,7 @@
 #parameters
 param = {
 
-    'T': 100000, 'K': 10000, 'epsilon':0.2,
-    'iter': 10,  #repeat times
+    'T': 100000, 'K': 10000, 'epsilon':0.3,
+    'iter': 50,  #repeat times
     'arm_type':0 #0: Gaussian, 1: Bernoulli 
 }
